6a87d1c8ac32d53268526700f05e1baef37d8780
Acetonerer <bolokan2014@gmail.com>
2024-10-24 13:08:09 +0700

    fix idiots

--- project/settings.py
+++ project/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
