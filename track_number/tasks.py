3d49efe06eca117778f8c34485d4c7a404292c1b
Acetonerer <bolokan2014@gmail.com>
2024-10-25 12:11:48 +0700

    test fix celery.py

--- track_number/tasks.py
+++ track_number/tasks.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 @shared_task
 def update_info_for_tracks():
     track_numbers = TrackNumber.objects.all()
+
     for track_number_obj in track_numbers:
         track_number = track_number_obj.track
         service = TrackService(track_number)
@@ -19,9 +20,14 @@ def update_info_for_tracks():
         if result["status"] == "success" and result["new_status"] != result["existing_status"]:
             logger.info(
                 f"Трек-номер {track_number}: статус изменился с {result['existing_status']} на {result['new_status']}")
-            bebraboi = TrackNumberAPIView()
-            bebraboi.create_amo_task(track_number, result['new_status'])
-            return "У нас новый крутой статус!"
+            # Создаем задачу для отправки в AmoCRM с задержкой
+            create_amo_task.apply_async((track_number, result['new_status']), countdown=30)
         else:
             logger.info(f"Трек-номер {track_number}: статус не изменился")
-            return "У нас старый, но тоже крутой статус!"
+
+
+@shared_task
+def create_amo_task(track_number, new_status):
+    # Логика создания задачи в AmoCRM
+    bebraboi = TrackNumberAPIView()
+    bebraboi.create_amo_task(track_number, new_status)
