463dd93e7d5b30e1a13be9ac7e56726cbf109873
Acetonerer <bolokan2014@gmail.com>
2024-10-25 11:12:10 +0700

    test change

--- track_number/tasks.py
+++ track_number/tasks.py
@@ -9,29 +9,19 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def update_info_for_tracks():
-    logger.info("Запуск задачи update_info_for_tracks")
     track_numbers = TrackNumber.objects.all()
-    logger.info(f"Найдено {track_numbers.count()} трек-номеров для обработки")
-
     for track_number_obj in track_numbers:
         track_number = track_number_obj.track
-        logger.info(f"Обработка трек-номера: {track_number}")
+        service = TrackService(track_number)
 
-        try:
-            service = TrackService(track_number)
-            result = service.process_tracking()
-            logger.info(f"Результат для трек-номера {track_number}: {result}")
-        except Exception as e:
-            logger.error(f"Ошибка при обработке трек-номера {track_number}: {e}")
-            continue
+        result = service.process_tracking()
 
         if result["status"] == "success" and result["new_status"] != result["existing_status"]:
-            try:
-                logger.info(f"Статус изменился, создание задачи для AmoCRM для трек-номера {track_number}")
-                bebraboi = TrackNumberAPIView()
-                bebraboi.create_amo_task(track_number, result['new_status'])
-                logger.info(f"Задача для AmoCRM успешно создана для трек-номера {track_number}")
-            except Exception as e:
-                logger.error(f"Ошибка при создании задачи для AmoCRM: {e}")
+            logger.info(
+                f"Трек-номер {track_number}: статус изменился с {result['existing_status']} на {result['new_status']}")
+            bebraboi = TrackNumberAPIView()
+            bebraboi.create_amo_task(track_number, result['new_status'])
+            return "У нас новый крутой статус!"
         else:
-            logger.info(f"Статус для трек-номера {track_number} не изменился")
+            logger.info(f"Трек-номер {track_number}: статус не изменился")
+            return "У нас старый, но тоже крутой статус!"
